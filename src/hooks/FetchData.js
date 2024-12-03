d915ca3fe8cf2d4d72188cbfce9802acca35c405
Vedant Parkhe <vedantparkhe2409@gmail.com>
2024-12-03 21:27:40 +0530

    first

---
+++ src/hooks/FetchData.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+
+const baseurl = "http://localhost:3000/";
+
+export const Home = async (page, limit, fullName) => {
+  try {
+    const response = await axios.get(
+      `${baseurl}api/users?page=${page}&limit=${limit}&fullname=${fullName}`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+export const Attr = async () => {
+  try {
+    const response = await axios.get(`${baseurl}attribute`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+export const AllUser = async (fullName) => {
+  try {
+    const response = await axios.get(
+      `${baseurl}api/allusers?fullname=${fullName}`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+export const Teams = async () => {
+  try {
+    const response = await axios.get(`${baseurl}api/teams`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+export const DeleteUser = async (userId) => {
+  try {
+    const response = await axios.delete(`${baseurl}api/users/${userId}`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    return null;
+  }
+};
+
+export const UpdateUser = async (userId, userData) => {
+  try {
+    const response = await axios.put(`${baseurl}api/users/${userId}`, userData);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error updating user:", error);
+    return null;
+  }
+};
